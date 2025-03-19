69b45854bffb4085d20d1c53023ad6ef98d9d7b2
251643 Emilia Szczerba <251643@edu.p.lodz.pl>
2025-03-19 15:25:51 +0100

    main

--- src/main.c
+++ src/main.c
@@ -5,29 +5,57 @@
 #include "../inc/myTemp.h"
 #include "../inc/myOled.h"
 #include "../inc/myJoystick.h"
+#include "lpc17xx_pinsel.h"
+#include "lpc17xx_gpio.h"
+#include "lpc17xx_adc.h"
+#include "LPC17xx.h"
+#include "../inc/eeprom.h"
 
 static uint32_t msTicks = 0;
 void SysTick_Handler(void) { msTicks++; }
 uint32_t getTicks(void) { return msTicks; }
 
-static void init_i2c(void) {
+void init_i2c(void) {
     PINSEL_CFG_Type PinCfg;
-    PinCfg.Funcnum = 1; PinCfg.Pinnum = 19; PinCfg.Portnum = 0;
+    // SCL (P0.19)
+    PinCfg.Funcnum = 3;
+    PinCfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+    PinCfg.Pinmode = PINSEL_PINMODE_PULLUP;
+    PinCfg.Pinnum = 19;
+    PinCfg.Portnum = 0;
     PINSEL_ConfigPin(&PinCfg);
-    PinCfg.Pinnum = 20;
+
+    // SDA (P0.27)
+    PinCfg.Pinnum = 27;
     PINSEL_ConfigPin(&PinCfg);
+
     I2C_Init(LPC_I2C1, 100000);
     I2C_Cmd(LPC_I2C1, ENABLE);
 }
 
-static void init_ssp(void) {
+void init_ssp(void) {
     PINSEL_CFG_Type PinCfg;
-    PinCfg.Funcnum = 2; PinCfg.Pinnum = 22; PinCfg.Portnum = 0;
+
+    // P0.7 - SCK1
+    PinCfg.Funcnum = 2;
+    PinCfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+    PinCfg.Pinmode = PINSEL_PINMODE_PULLUP;
+    PinCfg.Pinnum = 7;
+    PinCfg.Portnum = 0;
     PINSEL_ConfigPin(&PinCfg);
-    PinCfg.Pinnum = 23; PINSEL_ConfigPin(&PinCfg);
-    PinCfg.Pinnum = 24; PINSEL_ConfigPin(&PinCfg);
-    PinCfg.Pinnum = 28; PinCfg.Funcnum = 0;
+
+    // P0.8 - MISO1
+    PinCfg.Pinnum = 8;
     PINSEL_ConfigPin(&PinCfg);
+
+    // P0.9 - MOSI1
+    PinCfg.Pinnum = 9;
+    PINSEL_ConfigPin(&PinCfg);
+
+    // CS (chip select) – osobny GPIO, np. P0.6
+    LPC_GPIO0->FIODIR |= (1 << 6); // Ustaw jako wyjście
+    LPC_GPIO0->FIOSET |= (1 << 6); // Ustaw stan wysoki (nieaktywne)
+
     SSP_CFG_Type SSP_ConfigStruct;
     SSP_ConfigStructInit(&SSP_ConfigStruct);
     SSP_ConfigStruct.ClockRate = 1000000;
